c2ce5376c9eef9636feb254440acaee39f5698e5
peuchen <npeuchen@gmail.com>
2020-12-07 19:26:09 +0100

    Set up base case

--- merge-sort.rb
+++ merge-sort.rb
@@ -4,3 +4,10 @@
 #Sort the left half of the array (assuming n > 1)
 #Sort the right half of the array (assuming n > 1)
 #Merge the two halves together
+
+def merge_sort(array)
+  if array.length < 2
+    return array
+  else
+  end
+end
