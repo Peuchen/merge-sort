48f6ffab0990ab323d1d830380d4ae80b0ed61e2
peuchen <npeuchen@gmail.com>
2020-12-07 19:25:28 +0100

    Add pseudocode

---
+++ merge-sort.rb
@@ -0,0 +1,6 @@
+#Input n elements
+#Base case: If n < 2, then return the element
+#Else:
+#Sort the left half of the array (assuming n > 1)
+#Sort the right half of the array (assuming n > 1)
+#Merge the two halves together
