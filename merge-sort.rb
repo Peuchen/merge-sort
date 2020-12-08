b82f53f496c418bcd5d9101e54581b83d4245782
peuchen <npeuchen@gmail.com>
2020-12-08 15:06:04 +0100

    Set up merge_sort and merge methods

--- merge-sort.rb
+++ merge-sort.rb
@@ -1,13 +1,28 @@
-#Input n elements
-#Base case: If n < 2, then return the element
-#Else:
-#Sort the left half of the array (assuming n > 1)
-#Sort the right half of the array (assuming n > 1)
-#Merge the two halves together
-
 def merge_sort(array)
   if array.length < 2
-    return array
+    array
   else
+    left = merge_sort(array[0...(array.length/2)])
+    right = merge_sort(array[(array.length/2)..-1])
+    merge(left, right)
   end
 end
+
+def merge (left, right, array = [])
+  (left.length + right.length).times do
+    if left.empty?
+      array << right.shift
+    elsif right.empty?
+      array << left.shift
+    elsif left[0] < right[0]
+      array << left.shift
+    elsif left[0] > right[0]
+      array << right.shift
+    else
+      array << left.shift
+    end
+  end
+  array
+end
+
+p merge_sort([7,5,1,3,4,6,2])
